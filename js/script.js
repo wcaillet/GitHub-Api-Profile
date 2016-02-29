08629a18a7da5ca1bdd1fd6a5e7efc7038cd9707
Wensie <wcaillet@MacBook-Pro-3.local>
2016-02-29 16:28:02 -0600

    homework upload today

--- js/script.js
+++ js/script.js
@@ -4,7 +4,18 @@ console.log('howdy buddy')
 
 // ---------- The URLs we are using ------------//
 
-//var apiKey = '&38f5cc6257a6b39c836ca5e937e993a1e82f1ca5'
+// var apiKey = "?access_token=40541236a1882f4a5bb94aed45e4e1c3d30b7520"
+var searchBaseUrl = "https://api.github.com/users/"
+
+var profileUrl = "https://api.github.com/users/wcaillet"
+//var fullProfileUrl = profileUrl + apiKey 
+var profileEl = document.querySelector(".leftCol")
+
+var repoBaseUrl= "https://api.github.com/users/wcaillet/repos"
+var repoUrl = repoBaseUrl
+
+
+
 
 // ---------- Search ------------//
 // search input function that takes in search value and changes location.hash to that value
@@ -17,34 +28,51 @@ console.log('howdy buddy')
 //then runs seachjQuery function (the var from above)
 
 var inputEl = document.querySelector('.searchBox')
-inputEl.value = " "
-  
-var searchBaseUrl = "https://api.github.com/users/"
+inputEl.value = ""
   
 var doSearchRequest = function(userName) {
- 	var profileUrl = searchBaseUrl + userName //+ access key?
+ 	var profileUrl = searchBaseUrl + userName //+ apiKey
  	var userNamePromise = $.getJSON(profileUrl)  
   	
   	userNamePromise.then(handleProfileData)
 
-  	var repoUrl = searchBaseUrl + userName + "/repos" // + access key?
+  	var repoUrl = searchBaseUrl + userName + "/repos" //+ apiKey
   	var userRepoPromise = $.getJSON(repoUrl)
 
   	userRepoPromise.then(repoData)
 } 
+
+http://api.github.com/users/wcaillet/repos
   
 var inputToUrl = function(keyEvent) {
     var inputEl = keyEvent.target
     if (keyEvent.keyCode === 13) {
         var userName = inputEl.value
-        inputEl.value = ""
+        inputEl.value = ''
         location.hash = userName
     }
 }
 
+// Original code:
+// var controller = function() {
+//     var userName = location.hash.substring(1)
+//     doSearchRequest(userName)
+// }
+
 var controller = function() {
-    var userName = location.hash.substring(1)
-    doSearchRequest(userName)
+	var routeName = location.hash.substring(1)
+	if(routeName === "home") {
+		showHomeScreen()
+	}
+	else if(routeName === "settings") {
+		profileEl.innerHTML = "Welcome to the Settings page"
+	}
+	else doSearchRequest(routeName)
+}
+
+var showHomeScreen = function() {
+	profileEl.innerHTML = "Home on the profile page"
+	repoEl.innerHTML = "Repos will appear here"
 }
   
 // inputVar . addEventListener('keypress',search input)
@@ -52,15 +80,10 @@ var controller = function() {
 inputEl.addEventListener("keypress", inputToUrl)
 window.addEventListener('hashchange', controller)
 
+//location.hash = "wcaillet"
+
 
 // ---------- Profile Column ------------//
-
-var profileUrl = "https://api.github.com/users/wcaillet"
-//var fullProfileUrl = profileUrl + apiKey 
-
-var profilePromise = $.getJSON(profileUrl)
-
-var profileEl = document.querySelector(".leftCol")
 
 var profileData = function(jsonData) {
 	console.log(jsonData)
@@ -92,11 +115,6 @@ var handleProfileData = function(jsonProfileObject) {
 
 // ---------- Repo Column ------------//
 
-var repoBaseUrl= "https://api.github.com/users/wcaillet/repos"
-var repoUrl = repoBaseUrl
-
-var repoPromise = $.getJSON(repoUrl)
-
 var repoEl = document.querySelector(".repoListContainer")
 
 var repoData = function(jsonRepo){
@@ -123,11 +141,18 @@ var arrToHtml = function(repoObj) {
 
 // ---------- Declarations ------------//
 
-profilePromise.then(handleProfileData)
+// var profilePromise = $.getJSON(profileUrl)
+// profilePromise.then(handleProfileData)
 
-repoPromise.then(repoData)
+// var repoPromise = $.getJSON(repoUrl)
+// repoPromise.then(repoData)
 
-
+if (location.hash === ""){
+	location.hash = "home"
+} 
+else {
+ controller()
+}
 
 
 
